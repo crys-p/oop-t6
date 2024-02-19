09162259d929e81b8cb5b259b206efa0076d1d15
Janessa <janessang0312@gmail.com>
2024-02-19 18:26:31 +0800

    update mouse

--- core/src/com/mygdx/game/IOManager/Mouse.java
+++ core/src/com/mygdx/game/IOManager/Mouse.java
@@ -1,20 +1,62 @@
 package com.mygdx.game.IOManager;
 
-public class Mouse {
-	private boolean leftButton;
-	private boolean rightButton;
-	
-	public Mouse (boolean leftButton, boolean rightButton) {
-		this.leftButton = leftButton;
-		this.rightButton = rightButton;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JFrame;
+
+public class Mouse implements MouseListener {
+
+	JFrame frame = new JFrame();
+
+	public static void main(String[] args) {
+		new Mouse();
 	}
-	
-	public boolean isLeftButtonPressed() {
-		return true;
+
+	public Mouse() {
+		// Initialize your game or any setup here
+		initialize();
 	}
-	
-	public boolean isRightButtonPressed() {
-		return true;
+
+	public void initialize() {
+		// Add the mouse listener to your game component ->not sure about this part
+		// For example:
+		// gameComponent.addMouseListener(this);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit application
+		frame.setSize(1280, 720); //width, height
+		frame.setLocationRelativeTo(null); //center to the screen
+		frame.setVisible(true); //frame appear on screen
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// Handle mouse click event
+		int x = e.getX();
+		int y = e.getY();
+		System.out.println("Mouse Clicked at (" + x + ", " + y + ")");
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		System.out.println("You have pressed the mouse");
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		int x = e.getX();
+		int y = e.getY();
+		System.out.println("Mouse Released at (" + x + ", " + y + ")");
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		System.out.println("Mouse cursor enters bounds");
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		System.out.println("Mouse cursor out of bounds");
 	}
 
 }
