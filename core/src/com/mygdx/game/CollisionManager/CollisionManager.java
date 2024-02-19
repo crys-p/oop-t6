1c412ea10b7fea17197c83b982bdd6bd4a9c1f90
aaronio13 <luv.aaronchan@gmail.com>
2024-02-20 03:25:44 +0800

    collision manager test

--- core/src/com/mygdx/game/CollisionManager/CollisionManager.java
+++ core/src/com/mygdx/game/CollisionManager/CollisionManager.java
@@ -1,13 +1,22 @@
 package com.mygdx.game.CollisionManager;
 
+import com.mygdx.game.EntityManager.EntityManager;
+
 public class CollisionManager {
 
-    public CollisionManager()
+    private EntityManager entityManager;
+
+    public CollisionManager(EntityManager entityManager)
+    {
+        this.entityManager = entityManager;
+    }
+
+    public void CollisionDetection()
     {
 
     }
 
-    public void Collided()
+    public void createRect(float x, float y, float width, float height)
     {
 
     }
