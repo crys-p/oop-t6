c91a0c830c029996d4cc4967685b0cc61959924a
chuaedmund <edmundchua1996@gmail.com>
2024-02-20 12:16:57 +0800

    composition to SoundManager

--- core/src/com/mygdx/game/SoundManager/Sound.java
+++ core/src/com/mygdx/game/SoundManager/Sound.java
@@ -1,13 +1,15 @@
 package com.mygdx.game.SoundManager;
 
-public abstract class 	Sound {
-	private String filename;
+public abstract class Sound {
+	protected String filename;
 
 	public Sound(String filename) {
 		this.filename = filename;
 	}
 
-	public abstract void play();
-
+	// sound file have no usage as soundeffect and BackgroundMusic composition SoundManager
+	// can be remove. leave it here for now
+	public abstract void playSound();
 	public abstract void stop();
+	public abstract void dispose();
 }
--- core/src/com/mygdx/game/SoundManager/SoundManager.java
+++ core/src/com/mygdx/game/SoundManager/SoundManager.java
@@ -1,52 +1,34 @@
 package com.mygdx.game.SoundManager;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
-
-import java.util.Random;
-
 public class SoundManager {
-	private Music startSceneMusic;
-	private Music gameSceneMusic;
-	private Sound soundEffect;
+	private BackgroundMusic startSceneMusic;
+	private BackgroundMusic gameSceneMusic;
+	private SoundEffect soundEffect;
 
 	public SoundManager(String startSceneMusicFile, String gameSceneMusicFile, String soundEffectFile) {
-		// to load the start scene background music
-		startSceneMusic = Gdx.audio.newMusic(Gdx.files.internal(startSceneMusicFile));
-		// to load the game scene background music
-		gameSceneMusic = Gdx.audio.newMusic(Gdx.files.internal(gameSceneMusicFile));
-
-		// to load the sound effect
-		soundEffect = Gdx.audio.newSound(Gdx.files.internal(soundEffectFile));
+		startSceneMusic = new BackgroundMusic(startSceneMusicFile);
+		gameSceneMusic = new BackgroundMusic(gameSceneMusicFile);
+		soundEffect = new SoundEffect(soundEffectFile);
 	}
 
 	public void playStartSceneMusic() {
-		// start playing the start scene background music (looping)
-		startSceneMusic.setLooping(true);
 		startSceneMusic.play();
 	}
 
 	public void playGameSceneMusic() {
-		// start playing the game scene background music (looping)
-		gameSceneMusic.setLooping(true);
 		gameSceneMusic.play();
 	}
 
 	public void stopAllMusic() {
-		// stop playing all background music, can maybe split into stop Start and Game if i want
 		startSceneMusic.stop();
 		gameSceneMusic.stop();
 	}
 
 	public void playSoundEffect() {
-		// play the sound effect
 		soundEffect.play();
 	}
 
 	public void dispose() {
-		// dispose of all audio resources when they're no longer needed
-		// not use at all. hmmm dk
 		startSceneMusic.dispose();
 		gameSceneMusic.dispose();
 		soundEffect.dispose();
