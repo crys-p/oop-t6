01d545c137e15ac38df2faa62b2340bff00b7f51
janqj <janessang0312@gmail.com>
2024-02-21 04:58:55 +0800

    updated io, keyboard, input

--- core/src/com/mygdx/game/IOManager/Keyboard.java
+++ core/src/com/mygdx/game/IOManager/Keyboard.java
@@ -3,10 +3,6 @@ package com.mygdx.game.IOManager;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 
-import java.security.Key;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 class Keyboard{
@@ -31,27 +27,5 @@ class Keyboard{
 			input.receiveInput(Keys.DOWN);
 		}
 	}
-/*	protected boolean isKeyPressed(int keyCode) {
-		boolean keyPressed = Gdx.input.isKeyPressed(keyCode);
-
-		if (keyPressed) {
-			return true;
-		} else {
-			return false;
-		}
-	}*/
-
-	//should be called before updateInputs(in IOManager)
-/*	protected void updateKeys() {
-		keys.clear();
-		keys.put(Keys.UP, Gdx.input.isKeyPressed(Keys.UP));
-		keys.put(Keys.DOWN, Gdx.input.isKeyPressed(Keys.DOWN));
-		keys.put(Keys.LEFT, Gdx.input.isKeyPressed(Keys.LEFT));
-		keys.put(Keys.RIGHT, Gdx.input.isKeyPressed(Keys.RIGHT));
-	}*/
-
-/*	protected List<Integer> getKeys() {
-		return new ArrayList<>(keys.keySet());
-	}*/
 
 }
