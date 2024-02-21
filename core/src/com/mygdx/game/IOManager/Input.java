e1385c9639235add5dd81c92a436f5e50167ccac
janqj <janessang0312@gmail.com>
2024-02-21 14:26:38 +0800

    updated io

--- core/src/com/mygdx/game/IOManager/Input.java
+++ core/src/com/mygdx/game/IOManager/Input.java
@@ -15,57 +15,9 @@ class Input {
 		// Process input if needed
 		ioManager.updateMovement();
 	}
-	
-	/*protected Input(Keyboard keyboard, Mouse mouse) {
-		this.keyboard = keyboard;
-		this.mouse = mouse;
-	}
 
-	protected Keyboard getKeyboard() {
-		return keyboard;
-	}
-
-	protected void setKeyboard(Keyboard keyboard) {
-		this.keyboard = keyboard;
-	}
-
-	protected Mouse getMouse() {
-		return mouse;
-	}
-
-	protected void setMouse(Mouse mouse) {
-		this.mouse = mouse;
-	}
-
-	protected void update() {
-		mouse.update();
-		System.out.println("mouse update");
-
-		List<Integer> pressedKeys = getKeys();
-		System.out.println("keys update");
-
-		// Pass pressed keys to IOManager
-		IOManager.getInstance().handleInput(pressedKeys);
-	}
-
-	//retrieve keys as list
-<<<<<<< HEAD
-	public List<Integer> getKeys() {
-		List<Integer> pressedKeys = new ArrayList<>();
-		if (Gdx.input.isKeyPressed(Keys.UP))
-			pressedKeys.add(Keys.UP);
-		if (Gdx.input.isKeyPressed(Keys.DOWN))
-			pressedKeys.add(Keys.DOWN);
-		if (Gdx.input.isKeyPressed(Keys.LEFT))
-			pressedKeys.add(Keys.LEFT);
-		if (Gdx.input.isKeyPressed(Keys.RIGHT))
-			pressedKeys.add(Keys.RIGHT);
-		return pressedKeys;
-	}
-
-
-
-
-	protected abstract List<Integer> getKeys();*/
+	/*	protected void receiveMouseInput(int button) {
+		ioManager.updateMouse();
+	}*/
 
 }
